903f4ce44d8a2b4a0e7b03db7b38690cbca35a1e
Jitender Singh <73359865+sharpsailor@users.noreply.github.com>
2023-01-30 21:47:23 +0530

    Create Q_240_searchA2DMatrix_II.java

---
+++ src/Leetcode/Arrays/Q_240_searchA2DMatrix_II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return Search(matrix ,target);
+    }
+    public static boolean Search(int[][]arr,int item) {
+		int row =0;
+		int col = arr[0].length-1;
+		while(row<arr.length &&  col>=0) {
+			if(arr[row][col]==item) {
+				return true;
+			}else if(arr[row][col]>item) {
+				col--;
+			}else {
+				row++;
+			}
+		}
+		return false;
+	}
+}
